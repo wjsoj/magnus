3eca08b9b396d399b0f259215741a2870e9c65fa
parkcai <parkcai@stu.pku.edu.cn>
2026-01-13 14:13:14 +0800

    fix: minor change

--- back_end/server/database.py
+++ back_end/server/database.py
@@ -2,10 +2,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from pywheels.file_tools import guarantee_file_exist
 from ._magnus_config import *
 
 
 magnus_database_path = f"{magnus_config['server']['root']}/database"
+guarantee_file_exist(magnus_database_path, is_directory=True)
 sqlalchemy_database_url = f"sqlite:///{magnus_database_path}/magnus.db"
 
 

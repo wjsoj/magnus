82dd41a8b2e9e3eb2565c29d081b299670d5d3b7
parkcai <parkcai@126.com>
2026-02-17 00:44:10 +0800

    [jobs] fix: minor change of job runtime intepretation

--- back_end/server/_scheduler.py
+++ back_end/server/_scheduler.py
@@ -494,7 +494,7 @@ for _var in HTTP_PROXY HTTPS_PROXY ALL_PROXY http_proxy https_proxy all_proxy NO
     fi
 done
 
-APPTAINER_CMD="apptainer exec --nv --containall --overlay {{overlay_path}} --pwd /magnus/workspace/repository {{sif_path}} bash /magnus/workspace/.magnus_user_script.sh"
+APPTAINER_CMD="apptainer exec --nv --containall --no-mount tmp --overlay {{overlay_path}} --pwd /magnus/workspace/repository {{sif_path}} bash /magnus/workspace/.magnus_user_script.sh"
 
 if [ "${{{{MAGNUS_NET_MODE:-host}}}}" = "bridge" ]; then
     rootlesskit --net=slirp4netns --disable-host-loopback --port-driver=builtin --publish "$MAGNUS_PORT_MAP" $APPTAINER_CMD
